a4431248b3b09268ea4145ab5d19129e6d58a9a7
desha comp <deshacomp@hotmail.com>
2023-11-26 13:45:31 +0200

    Replace old domain project with another new

---
+++ MostafaProject.Domain/Dto/BaseAddDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MostafaProject.Domain.Dtos
+{
+    public class BaseAddDto
+    {
+        public DateTime Created_At { get; set; }
+        public Guid Created_by { get; set; }
+    }
+}
